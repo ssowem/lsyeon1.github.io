b3d9692db627451a51f7e7c9fb06fba56090f256
soyeon <lsyeon0825@gmail.com>
2023-10-25 19:08:11 +0900

    about 영역 수정

--- portfolio/mainsite/js/script.js
+++ portfolio/mainsite/js/script.js
@@ -1,41 +1,60 @@
-// 등장액션대상
-const aboutSection = document.querySelectorAll('.section')
-// 화면높이값 2/3 구하기
-const screenH = window.innerHeight / 3*2;
+window.onload = function(){
+  
+  // debugger;
+  const elm = document.querySelectorAll('.section');
+  const elmCount = elm.length;
+  // debugger;
+  elm.forEach(function(item, index){
+    // debugger;
+    item.addEventListener('mousewheel', function(event){
+      // debugger;
+      event.preventDefault();
+      let delta = 0;
 
-// 등장액션대상 위치값 리턴함수
-const retVal = ele => ele.getBoundingClientRect().top;
+      if (!event) event = window.event;
+      if (event.wheelDelta) {
+          delta = event.wheelDelta / 120;
+          if (window.opera) delta = -delta;
+      } 
+      else if (event.detail)
+          delta = -event.detail / 3;
 
-console.log(aboutSection)
-console.log(screenH)
-console.log("",retVal)
+      let moveTop = window.scrollY;
+      let elmSelector = elm[index];
 
-const showTit = x => {
-  let xval = retVal(x);
-  // if(xval < screenH && xval > 0){
-  //   x.classList.add("on");
-  // } 
-  if (xval > 0 && xval < screenH) { // xval이 양수일 때만 클래스 추가
-    x.classList.add("on");
-  } 
-};
+      // wheel down : move to next section
+      if (delta < 0){
+        if (elmSelector !== elmCount-1){
+          try{
+            moveTop = window.pageYOffset + elmSelector.nextElementSibling.getBoundingClientRect().top;
+          }catch(e){}
+        }
+      }
+      // wheel up : move to previous section
+      else{
+        if (elmSelector !== 0){
+          try{
+            moveTop = window.pageYOffset + elmSelector.previousElementSibling.getBoundingClientRect().top;
+          }catch(e){}
+        }
+      }
+
+      const body = document.querySelector('html');
+      window.scrollTo({top:moveTop, left:0, behavior:'smooth'});
+    });
+  });
+  fadeOut();
+}
 
 
-window.addEventListener("scroll",()=>{
-  // console.log("어바웃섹션",aboutSection)
-  // showTit(aboutSection)
-  for(let x of aboutSection)showTit(x);
- 
-});
+// window.addEventListener('load',function(){
+//   const loadingModal = document.getElementById('loading');
+//   const main = document.getElementById('maincontent')
+//   loadingModal.style.display = 'none';
+//   main.style.display = 'block';
+  
+// })
 
-// window.addEventListener("load", () => {
-//   window.scrollTo(0, 0); // 스크롤 위치를 (0, 0)으로 설정
-//   console.log("새로고침",aboutSection[0].classList);
-//   for(let x of aboutSection){
-//     console.log("x.클래스리스트",x.classList)
-//     x.classList.remove("on");
-//   };
-// });
 
 
 
@@ -75,10 +94,6 @@ window.onscroll = function(){
   } 
 }
 
-// $(window).scroll(function(){
-//   const bg = 
-// })
-
 
 var swiper = new Swiper(".content-slider", {
   spaceBetween: 100,
@@ -108,7 +123,7 @@ function fadeOut(){
   setInterval(loader, 3000);
 }
 
-window.onload = fadeOut;
+// window.onload = fadeOut;
 
 
 
